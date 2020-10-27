46b7a18421c984fbf9df46f62565263970c2c1d8
Lena <bunechka@inbox.lv>
2020-10-27 21:54:45 +0200

    amended code

--- src/test/java/Lesson10/ForumTest.java
+++ src/test/java/Lesson10/ForumTest.java
@@ -5,8 +5,8 @@ import Pages.ForumLogin;
 public class ForumTest {
     public static void main(String[] args) {
         ForumLogin page = new ForumLogin();
-        page.startPage("https://www.forumcinemas.lv/");
-        page.goToLogin();
+        page.startPage("https://www.forumcinemas.lv/mypage/logon/?returnUrl=%252f");
+        //page.goToLogin();
         page.login("bunechka@inbox.lv", "TestingForum");
         page.pressSubmitButton();
     }
