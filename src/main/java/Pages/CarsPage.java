847375a39597c36bf4eadb9035a10ba08905c079
Lena <bunechka@inbox.lv>
2020-10-27 18:59:55 +0200

    git comit

--- src/main/java/Pages/CarsPage.java
+++ src/main/java/Pages/CarsPage.java
@@ -9,6 +9,9 @@ public class CarsPage extends Common {
     private By carPriceMin = By.id("f_o_8_min");
     private By carPriceMax = By.id("f_o_8_max");
     private By minYear = By.id("f_o_18_min");
+    private By carColor = By.id("f_o_17");
+    private By engineVolume = By.id("f_o_34");
+    private By submitButton = By.cssSelector("input[value = 'Meklēt']");
 
     public void setPrice (String min, String max) {
         driver.findElement(carPriceMin).sendKeys(min);
@@ -18,6 +21,20 @@ public class CarsPage extends Common {
     public void selectMinYear (String year) {
         Select minYearSelector = new Select(driver.findElement(minYear));
         minYearSelector.selectByValue(year);
+    }
+
+    public void selectCarColor (String color) {
+        Select carColorSelector = new Select(driver.findElement(carColor));
+        carColorSelector.selectByValue(color);
+    }
+
+    public void selectEngine (String engine){
+        Select engineSelector = new Select(driver.findElement(engineVolume));
+        engineSelector.selectByValue(engine);
+    }
+
+    public void pressSearchButton () {
+        driver.findElement(submitButton).click();
     }
 
 }
