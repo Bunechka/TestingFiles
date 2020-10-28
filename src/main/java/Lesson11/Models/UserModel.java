6b7831bb3257328b2b91d3486f26d643a977ca4f
Lena <bunechka@inbox.lv>
2020-10-28 22:01:25 +0200

    amended code

---
+++ src/main/java/Lesson11/Models/UserModel.java
@@ -0,0 +1,15 @@
+package Lesson11.Models;
+
+public class UserModel {
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    private String username = "testing@inbox.lv";
+    private String password = "TestingProfile";
+
+}
